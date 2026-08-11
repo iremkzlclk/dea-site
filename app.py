1f79436a9f57d8269bbbb0a289070d2c2b7874f3
İrem Kızılçelik <kizilcelikirem@gmail.com>
2026-08-11 13:23:51 +0300

    kk

--- app.py
+++ app.py
@@ -1221,7 +1221,7 @@ if "sonuc" in st.session_state:
 
             pasta = (pasta_dilimleri + yuzde_etiketleri).properties(
                 width="container", height=750,
-                padding={"top": 55, "bottom": 15, "left": 40, "right": 40},
+                padding={"top": 75, "bottom": 15, "left": 40, "right": 40},
                 title=alt.TitleParams(
                     text="Değişkenlerin R²'ye Katkı Payı", subtitle="(mutlak payla ölçeklenmiştir)",
                     fontSize=24, subtitleFontSize=15, font="Georgia", color="#1F3A5F",
